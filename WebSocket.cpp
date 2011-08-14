74ae80af3974d1404ecc0135dd0fee8c8d11cd8c
Aaron Ford <aaronjamesford@gmail.com>
2011-08-14 21:35:29 +0100

    WebSocket::_handshake( ) is almost complete. I think it extracts most of the important fields. Now it needs to construct the response and send it. Added WebSocket::_getField( ) - a helper function to avoid repeated code in WebSocket::_handshake( ). It will search for and extract a given field from the header.

--- WebSocket.cpp
+++ WebSocket.cpp
@@ -7,22 +7,55 @@
 
 void WebSocket::_handshake( const std::string& header )
 {
-	// the last 8 bytes is easy peasy
-	_l8b = header.substr( header.length( ) - 8 );
+	// the request - i.e. "GET /uobnfsjldf HTTP1.1/1" or whatever the fuck it is
+	_req = header.substr( 0, header.find( '\n' ) );
+	
+	// the Host
+	_host = _getField( header, "Host" );
+	boost::algorithm::trim( _host );
+	
+	// the origin
+	_origin = _getField( header, "Origin" );
+	boost::algorithm::trim( _origin );
+	
+	// TODO : Probably add some more
 	
 	// get key 1
-	size_t pos = header.find( "Sec-WebSocket-Key1" );
-	_key1 = header.substr( header.find( ':', pos ), header.find( '\n', pos ) );
+	_key1 = _getField( header, "Sec-WebSocket-Key1" );
 	boost::algorithm::trim( _key1 );
 	
 	// get key2
-	pos = header.find( "Sec-WebSocket-Key2" );
-	_key2 = header.substr( header.find( ':', pos ), header.find( '\n', pos ) );
+	_key2 = _getField( header, "Sec-WebSocket-Key2" );
 	boost::algorithm::trim( _key2 );
+	
+	// the last 8 bytes is easy peasy
+	_l8b = header.substr( header.length( ) - 8 );
 	
 	// @TODO - get the rest of the fields
 	
 	std::string secret = _genSecret( );
+}
+
+std::string WebSocket::_getField( const std::string& header, const std::string& field )
+{
+	size_t start = header.find( field );
+	size_t colon;
+	size_t newline;
+	
+	if( start != std::string::npos )
+	{
+		colon = header.find( ':', start );
+		if( colon != std::string::npos )
+		{
+			newline = header.find( '\n', colon );
+			if( newline != std::string::npos )
+			{
+				return header.substr( colon + 1, newline );
+			}
+		}
+	}
+	
+	return "";
 }
 
 std::string WebSocket::_genSecret( )
--- WebSocket.h
+++ WebSocket.h
@@ -11,13 +11,15 @@ public:
 private:
 	int _port;
 	
+	std::string _req;
+	std::string _origin;
 	std::string _host;
 	std::string _key1;
 	std::string _key2;
 	std::string _l8b;
-	std::string _origin;
 	
 	void _handshake( const std::string& header );
+	std::string _getField( const std::string& header, const std::string& field );
 	std::string _genSecret( );
 	int _extractKey( const std::string& token );
 	std::string _getBigEndRep( int x );
