f223e1954bd3961e3b82a966bf93f3bfa8f645d2
Aaron Ford <aaronjamesford@gmail.com>
2011-08-15 21:22:18 +0100

    test.cpp creates an instance and runs it on port 2738 (forgot to account for the command line parameter. make file adds test target to create the executable for test.cpp TODO :   -- Move User from anonymous namespace - We have warnings and the programmer should be allowed to override. Can't do that outside the namespace if User is anonymous   -- Test for multiple clients - I have a feeling that it may block at some point.   -- General clean up of code

--- makefile
+++ makefile
@@ -5,6 +5,9 @@ _boost := /usr/lib/boost_1_44_0
 
 all : md5 websock
 
+test : md5 websock
+	g++ -O2 -I${_boost} WebSocket.cpp md5/md5.cpp test.cpp -L${_boost}/stage/lib -lboost_system -o test
+
 websock : ${_websock}
 	g++ -O2 -I${_boost} WebSocket.cpp -c
 
---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include "WebSocket.h"
+
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main( int argc, char** argv )
+{
+	if( argc != 2 )
+	{
+		std::cerr << "usage: test <port>" << std::endl;
+		return -1;
+	}
+	
+	boost::asio::io_service io_service;
+	WebSocket::WebSocket ws( io_service, 2738 );
+	ws.start( );
+	io_service.run( );
+	
+	return 0;
+}
