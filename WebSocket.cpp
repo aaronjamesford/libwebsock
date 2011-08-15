794b546471b49de80a7e477b161ae9c7dfc97917
Aaron Ford <aaronjamesford@gmail.com>
2011-08-15 10:12:07 +0100

    Added basics for the socket connections. Added the class to a namespace so the typedefs and usings are only in the scope of the namespace. WebSocket::start( ) needs more work. Could not get it to set the non-blocking option. May have to try and use acceptor::async_accept( ). Possible problems with the function - must adhere to certain prototype etc etc

--- WebSocket.cpp
+++ WebSocket.cpp
@@ -1,143 +1,174 @@
 #include <string>
 #include <iostream>
+
 #include <boost/algorithm/string/trim.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/asio.hpp>
 
 #include "md5/md5.h"
 #include "WebSocket.h"
 
-void WebSocket::_handshake( const std::string& header )
+namespace WebSocket
 {
-	// the request - i.e. "GET /uobnfsjldf HTTP1.1/1" or whatever the fuck it is
-	_req = header.substr( 0, header.find( '\n' ) );
-	
-	// the Host
-	_host = _getField( header, "Host" );
-	boost::algorithm::trim( _host );
-	
-	// the origin
-	_origin = _getField( header, "Origin" );
-	boost::algorithm::trim( _origin );
-	
-	// protocol
-	_protocol = _getField( header, "Sec-WebSocket-Protocol" );
-	boost::algorithm::trim( _protocol );
-	
-	// TODO : Probably add some more
-	
-	// get key 1
-	_key1 = _getField( header, "Sec-WebSocket-Key1" );
-	boost::algorithm::trim( _key1 );
-	
-	// get key2
-	_key2 = _getField( header, "Sec-WebSocket-Key2" );
-	boost::algorithm::trim( _key2 );
-	
-	// the last 8 bytes is easy peasy
-	_l8b = header.substr( header.length( ) - 8 );
-	
-	// TODO : validate all the fields gathered so far
-	
-	// get the secret :)
-	std::string secret = _genSecret( );
-	
-	// construct the response
-	// probably needs some more validation as some of these are optional and some may depend on static values
-	std::string response = "HTTP/1.1 101 WebSocket Protocol Handshake\r\n";
-	response += "Upgrade: WebSocket\r\nConnection: Upgrade\r\n";
-	response += "Sec-WebSocket-Origin: " + _origin + "\r\n";
-	response += "Sec-WebSocket-Location: ws://" + _host + _path + "\r\n";
-	response += "Sec-WebSocket-Protocol: " + _protocol + "\r\n";
-	response += 0x0D;
-	response += 0x0A;
-	response += secret;
-	
-	std::cout << response << std::endl;
-	
-	// TODO : send the response once the send method is implemented;
-}
 
-std::string WebSocket::_getField( const std::string& header, const std::string& field )
-{
-	size_t start = header.find( field );
-	size_t colon;
-	size_t newline;
-	
-	if( start != std::string::npos )
+	WebSocket::WebSocket( boost::asio::io_service& io_service )
+	 : _io_service( io_service )
 	{
-		colon = header.find( ':', start );
-		if( colon != std::string::npos )
-		{
-			newline = header.find_first_of( "\r\n", colon );
-			if( newline != std::string::npos )
-			{
-				return header.substr( colon + 1, (newline - colon) - 1 );
-			}
-		}
+		
 	}
 	
-	return "";
-}
-
-std::string WebSocket::_genSecret( )
-{
-	int k1 = _extractKey( _key1 );
-	int k2 = _extractKey( _key2 );
-	
-	return md5( _getBigEndRep( k1 ) + _getBigEndRep( k2 ) + _l8b, true );
-}
-
-int WebSocket::_extractKey( const std::string& token )
-{
-	unsigned int spaceCount = 0;
-	unsigned int res = 0;
-	
-	std::string::const_iterator it = token.begin( );
-	while( it != token.end( ) )
+	void WebSocket::start( )
 	{
-		if( *it == ' ' )
+		tcp::acceptor server_sock( _io_service );
+		tcp::endpoint ep( tcp::v4( ), _port );
+		
+		// open the socket, set it to non blocking and bind the socket
+		server_sock.open( ep.protocol( ) );
+		// server_sock.set_option( boost::asio::socket_base::non_blocking_io( true ) );
+		server_sock.bind( ep );
+				
+		while( true )
 		{
-			++spaceCount;
+			// accept the socket, if it actually accepted, add the socket to the list
+			// read shit, determine validity, process
 		}
-		else
+	}
+
+	void WebSocket::_handshake( const std::string& header )
+	{
+		// the request - i.e. "GET /uobnfsjldf HTTP1.1/1" or whatever the fuck it is
+		_req = header.substr( 0, header.find( '\n' ) );
+		
+		// the Host
+		_host = _getField( header, "Host" );
+		boost::algorithm::trim( _host );
+		
+		// the origin
+		_origin = _getField( header, "Origin" );
+		boost::algorithm::trim( _origin );
+		
+		// protocol
+		_protocol = _getField( header, "Sec-WebSocket-Protocol" );
+		boost::algorithm::trim( _protocol );
+		
+		// TODO : Probably add some more
+		
+		// get key 1
+		_key1 = _getField( header, "Sec-WebSocket-Key1" );
+		boost::algorithm::trim( _key1 );
+		
+		// get key2
+		_key2 = _getField( header, "Sec-WebSocket-Key2" );
+		boost::algorithm::trim( _key2 );
+		
+		// the last 8 bytes is easy peasy
+		_l8b = header.substr( header.length( ) - 8 );
+		
+		// TODO : validate all the fields gathered so far
+		
+		// get the secret :)
+		std::string secret = _genSecret( );
+		
+		// construct the response
+		// probably needs some more validation as some of these are optional and some may depend on static values
+		std::string response = "HTTP/1.1 101 WebSocket Protocol Handshake\r\n";
+		response += "Upgrade: WebSocket\r\nConnection: Upgrade\r\n";
+		response += "Sec-WebSocket-Origin: " + _origin + "\r\n";
+		response += "Sec-WebSocket-Location: ws://" + _host + _path + "\r\n";
+		response += "Sec-WebSocket-Protocol: " + _protocol + "\r\n";
+		response += 0x0D;
+		response += 0x0A;
+		response += secret;
+		
+		std::cout << response << std::endl;
+		
+		// TODO : send the response once the send method is implemented;
+	}
+
+	std::string WebSocket::_getField( const std::string& header, const std::string& field )
+	{
+		size_t start = header.find( field );
+		size_t colon;
+		size_t newline;
+		
+		if( start != std::string::npos )
 		{
-			int c = *it - '0';
-			if( c >= 0 && c <= 9 )
+			colon = header.find( ':', start );
+			if( colon != std::string::npos )
 			{
-				res = res * 10;
-				res = res + c;
+				newline = header.find_first_of( "\r\n", colon );
+				if( newline != std::string::npos )
+				{
+					return header.substr( colon + 1, (newline - colon) - 1 );
+				}
 			}
 		}
 		
-		++it;
+		return "";
 	}
-	
-	if( spaceCount == 0 )
-	{ // error time
-		return -1;
-	}
-	
-	return res / spaceCount;
-}
 
-std::string WebSocket::_getBigEndRep( int x )
-{
-	int test = 1;
-	char* endian = (char*)&test;
-	
-	if( endian[ 0 ] )
-	{ // little endian
-		x = (x>>24) | 
-			((x<<8) & 0x00FF0000) |
-			((x>>8) & 0x0000FF00) |
-			(x<<24);
-	}
-	
-	unsigned char* r = (unsigned char*)&x;
-	std::string res = "";
-	for( int i = 0; i < 4; i++ )
+	std::string WebSocket::_genSecret( )
 	{
-		res += r[ i ];
+		int k1 = _extractKey( _key1 );
+		int k2 = _extractKey( _key2 );
+		
+		return md5( _getBigEndRep( k1 ) + _getBigEndRep( k2 ) + _l8b, true );
 	}
-	
-	return res;
+
+	int WebSocket::_extractKey( const std::string& token )
+	{
+		unsigned int spaceCount = 0;
+		unsigned int res = 0;
+		
+		std::string::const_iterator it = token.begin( );
+		while( it != token.end( ) )
+		{
+			if( *it == ' ' )
+			{
+				++spaceCount;
+			}
+			else
+			{
+				int c = *it - '0';
+				if( c >= 0 && c <= 9 )
+				{
+					res = res * 10;
+					res = res + c;
+				}
+			}
+			
+			++it;
+		}
+		
+		if( spaceCount == 0 )
+		{ // error time
+			return -1;
+		}
+		
+		return res / spaceCount;
+	}
+
+	std::string WebSocket::_getBigEndRep( int x )
+	{
+		int test = 1;
+		char* endian = (char*)&test;
+		
+		if( endian[ 0 ] )
+		{ // little endian
+			x = (x>>24) | 
+				((x<<8) & 0x00FF0000) |
+				((x>>8) & 0x0000FF00) |
+				(x<<24);
+		}
+		
+		unsigned char* r = (unsigned char*)&x;
+		std::string res = "";
+		for( int i = 0; i < 4; i++ )
+		{
+			res += r[ i ];
+		}
+		
+		return res;
+	}
+
 }
--- WebSocket.h
+++ WebSocket.h
@@ -2,35 +2,45 @@
 #define WEBSOCKET_H
 
 #include <string>
-// #include <boost/asio.hpp>
+#include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
 
-// using boost::asio::ip::tcp;
-
-class WebSocket
+namespace WebSocket
 {
-public:
-	WebSocket();
-	WebSocket( int port );
-	~WebSocket( );
+
+	using boost::asio::ip::tcp;
+	typedef boost::shared_ptr< tcp::socket* > sock_ptr;
+	typedef boost::shared_ptr< tcp::acceptor* > accept_ptr;
+
+	class WebSocket
+	{
+	public:
+		WebSocket( boost::asio::io_service& io_service );
+		WebSocket( int port );
+		~WebSocket( );
+			
+		void start( );
+	private:
+		boost::asio::io_service& _io_service;
 		
-	void start( );
-private:
-	int _port;
-	
-	std::string _req;
-	std::string _origin;
-	std::string _host;
-	std::string _protocol;
-	std::string _key1;
-	std::string _key2;
-	std::string _l8b;
-	std::string _path;
-	
-	void _handshake( const std::string& header );
-	std::string _getField( const std::string& header, const std::string& field );
-	std::string _genSecret( );
-	int _extractKey( const std::string& token );
-	std::string _getBigEndRep( int x );
-};
+		int _port;
+		
+		std::string _req;
+		std::string _origin;
+		std::string _host;
+		std::string _protocol;
+		std::string _key1;
+		std::string _key2;
+		std::string _l8b;
+		std::string _path;
+		
+		void _handshake( const std::string& header );
+		std::string _getField( const std::string& header, const std::string& field );
+		std::string _genSecret( );
+		int _extractKey( const std::string& token );
+		std::string _getBigEndRep( int x );
+	};
+
+}
 
 #endif
