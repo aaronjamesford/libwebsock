7a9bc6fbd372901d4b8c754f190a5cfac3c262ad
Aaron Ford <aaronjamesford@gmail.com>
2011-08-18 15:19:32 +0100

    Attempted an asynchronous send function. To loop through and send messages in the queue of each user.  -- Does not send the message (possible deadlock?)  -- Error due to deleting a locked mutex / unlocking a delete mutex
     -- Reverted back to calling old WebSocket::_send( ) function in process( ) and _handshake( )
     -- Consider deleting asynchronous attempts and reverting completely

--- WebSocket.h
+++ WebSocket.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <vector>
+#include <queue>
 
 #include <boost/asio.hpp>
 #include <boost/shared_ptr.hpp>
@@ -18,14 +19,22 @@ namespace libwebsock
 	
 	struct User
 	{
+		// mutex for exclusive access to the resource
+		mutex_ptr mutex;
+		
+		// socket for sneding/recieving... obviously
 		sock_ptr sock;
+		
+		// this queue is for the messages that have to be sent to the User
+		std::queue< std::string > q;
+		
+		// to determing whether the user has handshaken
 		bool handshaken;
-		mutex_ptr mut;
 		
 		User( )
 		{
 			handshaken = false;
-			mut = mutex_ptr( new boost::mutex( ) );
+			mutex = mutex_ptr( new boost::mutex( ) );
 		}
 	};
 
@@ -36,7 +45,7 @@ namespace libwebsock
 		~WebSocket( ) { };
 			
 		void start( );
-	private:
+	protected:
 		boost::asio::io_service& _io_service;
 		accept_ptr _server_sock;
 		
@@ -44,16 +53,19 @@ namespace libwebsock
 		int _maxBytes;
 	
 		std::vector< User > _users;
+		std::vector< User >::iterator _userIt;
 		boost::mutex  _userMutex;
 	
 		void _read( User& u );
 		void _accept( );
+		void _asyncSend( );
 	
 		void _handshake( User& u, std::string header );
 		
 		void _process( User& u, std::string req );
 		
 		void _send( sock_ptr sock, const std::string& resp );
+		void _send( User& u, const std::string& resp );
 	};
 
 }
